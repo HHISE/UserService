bcdd47f39f932507d90b6e6c74cdf5ba9206762a
selale920 <selaleg8@gmail.com>
2026-09-24 13:57:30 +0200

    user model rettet

--- Models/User.cs
+++ Models/User.cs
@@ -4,13 +4,12 @@ public class User
 {
     [Required]
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Address1 { get; set; }
-    public string? Address2 { get; set; }
-    public short PostalCode { get; set; }
-    public string? City { get; set; }
+
+    public string Name { get; set; } = string.Empty; 
+    public string Address1 { get; set; } = string.Empty; 
+    public string City { get; set; } = string.Empty; 
 
     [Required]
-    public string? EmailAddress { get; set; }
-    public string? PhoneNumber { get; set; }
+    public string EmailAddress { get; set; } = string.Empty; 
+    public string PhoneNumber { get; set; } = string.Empty; 
 }
